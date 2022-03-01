9af32dbb8a7eca5c7481739887269569c169b00c
buslik <nbuslaev35@gmail.com>
2022-03-01 10:11:16 +0200

    added db parameters

--- connector.js
+++ connector.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "yourusername",
-    password: "yourpassword"
+    host: "sql11.freesqldatabase.com",
+    user: "sql11476086",
+    password: "id7B3V3qax",
+    database: "sql11476086"
   });
 
 module.exports = {con} 

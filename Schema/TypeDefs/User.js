1ac795d9b9b47fbceb31f62bcb93b573bebb3774
Ivan Niukanen <ivanniu2003@gmail.com>
2022-03-01 08:45:59 +0200

    Resolvers are ready for querying

--- Schema/TypeDefs/User.js
+++ Schema/TypeDefs/User.js
@@ -1,11 +1,24 @@
 const { gql } = require("apollo-server-express");
 const UserTypes = gql`
-    type User { 
-        name: String
+    type Tag { 
+        id: Int!
+        name: String!
     }
-
+    type User {
+        id: Int!
+        email: String!
+        first_name: String!
+        last_name: String!
+        hashedPassword: String!
+        salt: Int!
+        birthday: String!
+        location: String! 
+        tags: [Tag!]!
+    }
+    
     type Query { 
-        getAllUsers: User
+        getAllUsers: User!
+        getUserById(id: ID): [User!]!
     }
 `
 

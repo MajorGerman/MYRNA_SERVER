13d1b0d2cb719cb5e3cea97f72acac0bf400bc4b
busliksuslik <nbuslaev35@gmail.com>
2022-03-04 11:08:52 +0200

    added .env file

--- connector.js
+++ connector.js
@@ -1,12 +1,13 @@
 var mysql = require('mysql2/promise');
+require('dotenv').config()
 
 const pool = mysql.createPool({
-    host: "localhost", 
-    user: "my_test",
-    password: "my_test",
-    database: "my_test",
+    host: process.env.DB_host, 
+    user: process.env.DB_user,
+    password: process.env.DB_password,
+    database: process.env.DB_database,
     connectionLimit: 10,
-    port: 3306
+    port: process.env.DB_port
   });
 
 

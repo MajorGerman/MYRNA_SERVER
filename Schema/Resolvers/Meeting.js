e34acd99d9ee76914031c7c0663cd942011da378
busliksuslik <nbuslaev35@gmail.com>
2022-06-07 08:29:07 +0200

    Bug: edit meeting trouble. Fixed

--- Schema/Resolvers/Meeting.js
+++ Schema/Resolvers/Meeting.js
@@ -42,6 +42,25 @@ const MeetingResolvers = {
             return true
         },
         changeMeeting: async (_, {meeting_id, name, date}, ctx) => {
+            const checkIfUserInMeeting = (user_id, members)=>{
+                for (i of members) {
+                    if (i.id == user_id){
+                        return true;
+                    }
+                    return false
+                } 
+            } 
+            try{
+                const user = verify(ctx.req.headers['verify-token'], process.env.SECRET_WORD).user;
+                
+                if (!isRolesInUser(await getUserRoles(user.id), ["ADMIN"]) 
+                && !checkIfUserInMeeting(user.id, await MeetingQueries.getAllMeetingMembers(meeting_id)))
+                    throw Error("You do not have rights (basically woman)")
+
+            } catch (err){
+                throw Error("You do not have rights (basically woman)")
+            }
+            
             MeetingQueries.changeMeeting(meeting_id, name, date);
             return MeetingQueries.getMeetingById(meeting_id)
         }

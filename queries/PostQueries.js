094d600c11e7af82d145488a866d4d2ce1911c66
busliksuslik <nbuslaev35@gmail.com>
2022-06-07 08:57:06 +0200

    check if comment deleted

--- queries/PostQueries.js
+++ queries/PostQueries.js
@@ -2,10 +2,10 @@ const queryTool = require('../tools/QueryTool')
 const {pool} = require("../connector");
 
 const getAllUserPosts = (user_id) =>{
-    return queryTool.getMany(pool, `SELECT * FROM posts WHERE author = ${user_id}`)
+    return queryTool.getMany(pool, `SELECT * FROM posts WHERE deleted = 0 AND author = ${user_id}`)
 }
 const getAllUserComments = (user_id) => {
-    return queryTool.getMany(pool, `SELECT * FROM comments WHERE author = ${user_id}`)
+    return queryTool.getMany(pool, `SELECT * FROM comments WHERE deleted = 0 AND author = ${user_id}`)
 }
 const deletePost = (post_id) => {
     queryTool.insert(pool, `UPDATE posts SET deleted = true WHERE id = ${post_id}`)
@@ -14,16 +14,16 @@ const deleteComment = (comment_id) => {
     queryTool.insert(pool, `UPDATE comments SET deleted = true WHERE id = ${comment_id}`)
 }
 const getPostById = (post_id) => {
-    return queryTool.getOne(pool, `SELECT * FROM posts WHERE id = ${post_id}`)
+    return queryTool.getOne(pool, `SELECT * FROM posts WHERE deleted = 0 AND id = ${post_id}`)
 }
 const getCommentById =(comment_id) =>{
-    return queryTool.getOne(pool, `SELECT * FROM comments WHERE id = ${comment_id}`)
+    return queryTool.getOne(pool, `SELECT * FROM comments WHERE deleted = 0 AND id = ${comment_id}`)
 }
 const getAllPosts = () =>{
     return queryTool.getMany(pool,`SELECT * FROM posts WHERE deleted = 0 ORDER BY id DESC`)
 }
 const getPostsByUserId = (user_id) => {
-    return queryTool.getMany(pool,`SELECT * FROM posts WHERE author = ${user_id}`)
+    return queryTool.getMany(pool,`SELECT * FROM posts WHERE deleted = 0 AND author = ${user_id}`)
 }
 const getSubscribedPosts = (user_id) => {
     return queryTool.getMany(pool,`

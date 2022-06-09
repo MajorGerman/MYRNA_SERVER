7905cbe8361b9711a45353f7f457117b65c41007
busliksuslik <nbuslaev35@gmail.com>
2022-06-09 12:20:06 +0200

    make chief protection 2

--- Schema/Resolvers/Meeting.js
+++ Schema/Resolvers/Meeting.js
@@ -108,7 +108,7 @@ const MeetingResolvers = {
             } 
             const user = verify(ctx.req.headers['verify-token'], process.env.SECRET_WORD).user;
 
-            console.log(await MeetingQueries.getAllMeetingMembers(meeting_id))
+            console.log(MeetingQueries.getMeetingById(meeting_id).chief)
             if (!(isRolesInUser(await UserQueries.getAllUserRoles(user.id), ["ADMIN"]) 
             || (MeetingQueries.getMeetingById(meeting_id).chief == user.id)))
                 throw Error("You do not have rights (basically woman)")
